e848d2181d2dbf5d92703d614f91e48105a772f5
ZaiGanZeRi <43492987+ZaiGanZeRi@users.noreply.github.com>
2019-04-10 20:16:02 +0800

    Update 矩阵取数.cpp

--- 矩阵取数.cpp
+++ 矩阵取数.cpp
@@ -1,15 +1,14 @@
 #include<iostream>
-#include<cstring>
 #include<cmath>
 #include<algorithm>
 #define MAXN 80
 using namespace std;
 int a[MAXN][MAXN];
-long long f[MAXN][MAXN];
-int o[MAXN],e[MAXN];
-long long ans;
-int d;
-int ma;
+long long f[MAXN][MAXN][MAXN][MAXN];
+
+long long ans[MAXN];
+
+
 int main() {
 	int n,m;
 	cin>>n>>m;
@@ -18,40 +17,26 @@ int main() {
 			cin>>a[i][j];
 	}
 	for(int i=1; i<=n; i++) {
-		o[i]=1;
-		e[i]=m;
-	}
-	for(int i=1; i<=n; i++) {
-		if(a[i][1]<+a[i][m]) {
-			f[1][1]+=a[i][1]*2;
-			o[i]=2;
-		} else {
-			f[1][m]+=a[i][m]*2;
-			e[i]=m-1;
-		}
-	}
-
-
-	for(int i=1; i<=n; i++) {
-
-		for(d=2; d<=m; d++) {
-
-			for(int j=o[i]; j<=e[i]; j++) {
-
-				if(a[i][j-1]<=a[i][m+j-d]) {
-					f[d][j]+=f[d-1][j-1]+a[i][j]*pow(2,d);
-
-				} else {
-					f[d][j]+=f[d-1][m+j-d]+a[i][j]*pow(2,d);
+		for(int l=1; l<=m; l++) {
+			for(int d=1; d<=m; d++) {
+				for(int j=1; j<=m; j++) {
+					if(j<l) 
+						f[i][l][d][j]=max(f[i][l][d-1][j-1],f[i][l][d-1][m-d+j+1])+a[i][j]*pow(2,d);
+					if(j>l) 
+						f[i][l][d][j]=max(f[i][l][d-1][j+d-m-1],f[i][l][d-1][j+1])+a[i][j]*pow(2,d);
+					if(j==l)f[i][l][d][j]=max(f[i][l][d-1][j-1],f[i][l][d-1][j+1])+a[i][j]*pow(2,d);
 
 				}
 			}
 		}
 	}
-	for(int i=1; i<=m; i++) {
-		ans=max(ans,f[m][i]);
-		cout<<f[m][i]<<endl;
+	for(int i=1; i<=n; i++) {
+		for(int l=1; l<=m; l++) {
+			ans[i]=max(ans[i],f[i][l][m][l]);
+			
+		}
+		ans[MAXN]+=ans[i];
 	}
-	cout<<ans<<endl;
+	cout<<ans[MAXN]<<endl;
 	return 0;
 }
